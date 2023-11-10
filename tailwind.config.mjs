70076b062bd772a6ab11bc35d7b1ca64020d7451
Igor Oliveira <57506257+iguoliveira@users.noreply.github.com>
2023-11-10 08:55:13 -0300

    Dev navbar (#3)
    * FEAT add bg color to tailwind config
    
    * FEAT add new SVGs to the project
    
    * FEAT finish sidebar base
    
    * FEAT add sidebar globally to the project
    
    * FIX align text in the middle of screen

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        "main-bg-color": "#1C1C1C"
+      },
       fontFamily: {
         sans: ["Nunito Variable", ...defaultTheme.fontFamily.sans],
       },
