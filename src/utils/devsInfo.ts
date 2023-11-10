80b4b526cac23b3a405f5aa4661187d68cf53ac0
iguoliveira <igor.or042@gmail.com>
2023-11-10 15:29:50 -0300

    FEAT testing

--- src/utils/devsInfo.ts
+++ src/utils/devsInfo.ts
@@ -1,1 +1,17 @@
-export const developers = [{ ventura: { title: "mama" } }];
+export const developers: { name: string; slogan: string; image: string }[] = [
+  {
+    name: "Vinicius Ventura",
+    slogan: "Ação transforma visão em realidade",
+    image: "https://avatars.githubusercontent.com/u/58307012?v=4",
+  },
+  {
+    name: "Guilherme Abe",
+    slogan: "Compartilhando Sucessos desde 2022",
+    image: "https://avatars.githubusercontent.com/u/58307012?v=4",
+  },
+  {
+    name: "Viktor Pereira",
+    slogan: "cargo run",
+    image: "https://avatars.githubusercontent.com/u/58307012?v=4",
+  },
+];
