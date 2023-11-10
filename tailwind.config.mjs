e8184b9dcb600d12e04f330c193f41a96a10dcbe
abehidek <hidek.abe@outlook.com>
2023-11-09 21:50:54 -0300

    feat: add AnimatedTextGradient component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,19 @@ export default {
       fontFamily: {
         sans: ["Nunito Variable", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
+      },
     },
   },
   plugins: [],
