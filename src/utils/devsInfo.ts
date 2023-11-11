f8e781bb46d537d94819754fbc0b975a154e902c
iguoliveira <igor.or042@gmail.com>
2023-11-11 03:12:38 -0300

    FEAT developers first info

--- src/utils/devsInfo.ts
+++ src/utils/devsInfo.ts
@@ -7,11 +7,26 @@ export const developers: { name: string; slogan: string; image: string }[] = [
   {
     name: "Guilherme Abe",
     slogan: "Compartilhando Sucessos desde 2022",
-    image: "https://avatars.githubusercontent.com/u/58307012?v=4",
+    image: "https://avatars.githubusercontent.com/u/39712525?v=4",
   },
   {
     name: "Viktor Pereira",
     slogan: "cargo run",
-    image: "https://avatars.githubusercontent.com/u/58307012?v=4",
+    image: "https://avatars.githubusercontent.com/u/56888067?v=4",
+  },
+  {
+    name: "Igor Oliveira",
+    slogan: "De rondonia pro mundo!",
+    image: "https://avatars.githubusercontent.com/u/57506257?v=4",
+  },
+  {
+    name: "Gustavo Baltazar",
+    slogan: "Aprovando tudo",
+    image: "https://avatars.githubusercontent.com/u/69852751?v=4",
+  },
+  {
+    name: "Vinicius Prates",
+    slogan: "Classic life style",
+    image: "https://avatars.githubusercontent.com/u/89698942?v=4",
   },
 ];
