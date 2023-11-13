974e57f584a4ed548a6b8b151e481173605b64bb
abehidek <hidek.abe@outlook.com>
2023-11-13 09:26:20 -0300

    test: cron job every minute to aggregate rss feeds
    - after aggregating, save to @vercel/kv "posts" key
    - on /content page, fetch from @vercel/kv  "posts" key

---
+++ src/pages/api/cron/aggregate.ts
@@ -0,0 +1,73 @@
+import { createClient } from "@vercel/kv";
+import { getDevs } from "../../../utils";
+import Parser from "rss-parser";
+import ogs from "open-graph-scraper";
+
+export type Post = {
+  title: string | undefined;
+  description: string | undefined;
+  link: string | undefined;
+  cover: string | undefined;
+  date: string | undefined;
+  author: {
+    name: string;
+    image: string;
+  };
+};
+
+export const prerender = false;
+
+const kv = createClient({
+  url: import.meta.env.KV_REST_API_URL,
+  token: import.meta.env.KV_REST_API_TOKEN,
+});
+
+export async function GET() {
+  let parser = new Parser();
+  const devs = getDevs();
+  const posts: Post[] = (
+    await Promise.all(
+      devs.map(async (dev) => {
+        if (dev.rss) {
+          let feed = await parser.parseURL(dev.rss);
+
+          return await Promise.all(
+            feed.items.map(async (item) => {
+              const imageUrl = item.enclosure
+                ? item.enclosure.url
+                : (await ogs({ url: item.link })).result.ogImage?.at(0)?.url;
+              return {
+                title: item.title,
+                description: item.content,
+                link: item.link,
+                cover: imageUrl,
+                date: item.pubDate,
+                author: {
+                  name: dev.name,
+                  image: dev.image,
+                },
+              };
+            })
+          );
+        }
+        return [];
+      })
+    )
+  )
+    .flat()
+    .sort(
+      (a, b) =>
+        (new Date(b.date as string) as unknown as number) -
+        (new Date(a.date as string) as unknown as number)
+    );
+
+  const response = await kv.set("posts", posts);
+
+  const now = Date.now();
+  return new Response(JSON.stringify({ response, now }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
