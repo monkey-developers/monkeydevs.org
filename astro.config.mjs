2b821200c0d8ebb7189f6bf37c19d5ccc0509ecb
abehidek <hidek.abe@outlook.com>
2023-11-13 09:01:46 -0300

    fix: add vercel adapter for hybrid server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "hybrid",
+  adapter: vercel()
 });
